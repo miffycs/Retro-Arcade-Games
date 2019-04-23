5e03b0093d1c126041bfad42cc76ceb3a58756aa
Miffy Chen <mchen15@bu.edu>
2019-04-23 03:21:04 -0400

    Moved Old Tetris Made with Eclipse into legacy folder

---
+++ Tetris_Eclipse/src/tetris/About.java
@@ -0,0 +1,105 @@
+package tetris;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+
+
+public class About extends JFrame {
+
+    private GroupLayout layout;
+    private JLabel projectTitle;
+    private JLabel projectID;
+    private JLabel className;
+    private JLabel author;
+    private JButton exit;
+
+    public About() {
+        initComponents();
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("About");
+        getContentPane().setLayout(layout);
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    private void initComponents() {
+        initLabels();
+        initButtons();
+        initLayout();
+    }
+
+    private void initLabels() {
+        projectTitle = new JLabel("Tetris");
+        projectTitle.setFont(new Font("Lucida Grande", Font.BOLD, 30));
+        projectID = new JLabel("CS622 Final Project");
+        projectID.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+        className = new JLabel("Advance Programming in Java");
+        className.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+        author = new JLabel("by Miffy Chen © 2019");
+        author.setFont(new Font("Lucida Grande", Font.BOLD, 20));
+    }
+
+    private void initButtons() {
+        exit = new JButton("Exit");
+        exit.addActionListener((ActionEvent evt) -> {
+            exitActionPerformed(evt);
+        });
+        // non-lambda
+        /*
+        exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                exitActionPerformed(evt);
+            }
+        });
+        */
+    }
+
+    private void initLayout() {
+        layout = new GroupLayout(getContentPane());
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(Alignment.CENTER)
+                        .addGroup(layout.createSequentialGroup().addGap(400))
+                        .addGroup(layout.createSequentialGroup().addComponent(projectTitle))
+                        .addGroup(layout.createSequentialGroup().addComponent(projectID))
+                        .addGroup(layout.createSequentialGroup().addComponent(className))
+                        .addGroup(layout.createSequentialGroup().addComponent(author))
+                        .addGroup(layout.createSequentialGroup().addComponent(exit)));
+        layout.setVerticalGroup(
+                layout.createParallelGroup(Alignment.CENTER)
+                        .addGroup(layout.createSequentialGroup().addGap(30)
+                                .addComponent(projectTitle).addGap(20)
+                                .addComponent(projectID).addGap(20)
+                                .addComponent(className).addGap(20)
+                                .addComponent(author).addGap(20)
+                                .addComponent(exit).addGap(30)));
+    }
+
+    private void exitActionPerformed(ActionEvent evt) {
+        this.dispose();
+    }
+
+    public static void main(String args[]) {
+        EventQueue.invokeLater(() -> {
+            new About().setVisible(true);
+        });
+        
+        // non-lambda
+        /*
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new About().setVisible(true);
+            }
+        });        
+        */
+    }
+
+}
---
+++ Tetris_Eclipse/src/tetris/HowToPlay.java
@@ -0,0 +1,135 @@
+package tetris;
+
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.JTableHeader;
+
+
+public class HowToPlay extends JFrame {
+
+    private GroupLayout layout;
+    private JLabel title;
+    private JLabel desc;
+    private JLabel controls;
+    private JTable controlsTable;
+    private JScrollPane scrollPane;
+    private JButton back;
+
+    public HowToPlay() {
+        initComponents();
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("How To Play");
+        getContentPane().setLayout(layout);
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    private void initComponents() {
+        initLabels();
+        initButtons();
+        initLayout();
+    }
+
+    private void initLabels() {
+        title = new JLabel("How To Play");
+        title.setFont(new Font("Lucida Grande", Font.BOLD, 30));
+
+        desc = new JLabel("<html>"
+                + "Tetriminos, shapes made up of 4 blocks of squares blocks,<br>"
+                + "&nbsp &nbsp will fall in a random sequence from the top of the board all the way down.<br>"
+                + "<br>"
+                + "Move the shapes sideways, rotate, or drop them to form a horizontal line<br>"
+                + "&nbsp &nbsp with no gaps to clear the line.<br>"
+                + "<br>"
+                + "Players are awarded <b>1 point per lines cleared</b>."
+                + "</html>");
+        desc.setHorizontalAlignment(SwingConstants.CENTER);
+        desc.setHorizontalTextPosition(SwingConstants.CENTER);
+
+        controls = new JLabel("Controls");
+        controls.setFont(new Font("Lucida Grande", Font.BOLD, 26));
+
+        String[] columnNames = {"Function", "Key"};
+        String[][] data = {
+            {"   Move Left", "←"},
+            {"   Move Right", "→"},
+            {"   Rotate Piece", "↑"},
+            {"   Move Down", "↓"},
+            {"   Drop Piece", "[space]"},
+            {"   Pause Game", "p"}
+        };
+        controlsTable = new JTable(data, columnNames);
+        controlsTable.setFillsViewportHeight(true);
+        JTableHeader tableHeader = controlsTable.getTableHeader();
+        tableHeader.setFont(new Font("Lucida Grande", Font.BOLD, 12));
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        ((DefaultTableCellRenderer) tableHeader.getDefaultRenderer()).setHorizontalAlignment(JLabel.CENTER);
+        centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+        controlsTable.getColumnModel().getColumn(1).setCellRenderer(centerRenderer);
+
+        scrollPane = new JScrollPane(controlsTable);
+        scrollPane.setPreferredSize(new Dimension(200, 100));
+    }
+
+    private void initButtons() {
+        back = new JButton("Back");
+        back.addActionListener((ActionEvent evt) -> {
+            exitActionPerformed(evt);
+        });
+    }
+
+    private void initLayout() {
+
+        layout = new GroupLayout(getContentPane());
+
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(Alignment.CENTER)
+                        .addGap(600)
+                        .addComponent(title)
+                        .addComponent(desc)
+                        .addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 290, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(back)
+                        .addComponent(controls)
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(Alignment.CENTER)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(30)
+                                .addComponent(title)
+                                .addGap(20)
+                                .addComponent(desc)
+                                .addGap(30)
+                                .addComponent(controls)
+                                .addGap(10)
+                                .addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE)
+                                .addGap(20)
+                                .addComponent(back)
+                                .addGap(30))
+        );
+
+    }
+
+    private void exitActionPerformed(ActionEvent evt) {
+        this.dispose();
+    }
+
+    public static void main(String args[]) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new HowToPlay().setVisible(true);
+            }
+        });
+    }
+}
